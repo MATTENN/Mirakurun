7993f84eff0159645705f08173c46d4816952d2e
stuayu <60843145+stuayu@users.noreply.github.com>
2024-02-11 16:57:34 +0900

    Fix: 重複チャンネルの処理を修正

--- src/Mirakurun/ChannelItem.ts
+++ src/Mirakurun/ChannelItem.ts
@@ -183,6 +183,13 @@ export default class ChannelItem {
                 const item = _.service.get(service.networkId, service.serviceId);
                 // すでに同一サービスが存在している
                 if (item !== null) {
+                    // タイプとチャンネルが一致した場合スキップする
+                    for (let index = 0; index < item.channel.length; index++) {
+                        if (service.channel[0].type == item.channel[index].type &&
+                            service.channel[0].channel == item.channel[index].channel) {
+                            return;
+                        }
+                    }
                     item.name = service.name;
                     item.type = service.type;
                     if (service.logoId > -1) {
